b42a5a65b264685b4652b703e412b09eb9245ce7
Aline Bevilacqua <aline.bevilacqua@hotmail.com.br>
2024-07-01 22:39:03 -0300

    feat: função para atualizar as informações das mulheres

--- components/WomanCard.vue
+++ components/WomanCard.vue
@@ -1,18 +1,48 @@
 <template>
-    <li>
-        <img :src="props.woman.image" :alt="'Imagem de perfil da ' + props.woman.name" />
+    <li class="woman-card">
+        <img :src="props.woman.image" :alt="'Imagem de perfil da ' + props.woman.name" class="woman-img" />
         <details>
             <summary>{{ props.woman.name }}</summary>
-            <p>{{ props.woman.desc }}</p>
+            <p class="woman-description">{{ props.woman.desc }}</p>
         </details>
+        <button class="secondary-btn" @click="openDialog">Editar</button>
+        <dialog ref="dialog" class="edit-dialog">
+            <form @submit.prevent="updateWoman(name, image, desc, props.woman._id)">
+                <label>Nome: <input type="text" v-model="name" class="form-input" /></label>
+                <label>Url da imagem: <input type="text" v-model="image" class="form-input" /></label>
+                <label>Descrição: <textarea v-model="desc" class="form-input" rows="5"></textarea></label>
+                <button type="submit" class="primary-btn" :disabled>Atualizar</button>
+            </form>
+        </dialog>
     </li>
 </template>
 <script setup lang="ts">
+import { ref } from 'vue';
 import type { Woman } from '../model/Woman';
 const props = defineProps<{ woman: Woman }>();
+const dialog = ref(null);
+const name = ref(null);
+const image = ref(null);
+const desc = ref(null);
+const disabled = ref(false);
+
+function openDialog() {
+    dialog.value!.showModal();
+}
+
+async function updateWoman(name: string | null, image: string | null, desc: string | null, id: string | undefined) {
+    disabled.value = true;
+    const body = {};
+    if (name) body.name = name;
+    if (image) body.image = image;
+    if (desc) body.desc = desc;
+
+    await $fetch(`https://backend-teste-vn7a.onrender.com/mulheres/${id}`, { method: 'PATCH', body });
+    reloadNuxtApp();
+}
 </script>
 <style scoped>
-li {
+.woman-card {
     color: #fdfdfd;
     background-color: var(--main-color);
     border-radius: 30px;
@@ -23,14 +53,58 @@ li {
     padding: 1.5rem;
 }
 
-img {
+.woman-img {
     border-radius: 100%;
     width: 130px;
     min-height: 130px;
     height: 130px;
 }
 
-details p {
+.woman-description {
     font-size: 13px;
+}
+
+.form-input {
+    display: block;
+    border-radius: 10px;
+    border: 2px solid var(--main-color);
+    width: 100%;
+    padding: 8px;
+    margin-top: 8px;
+    margin-bottom: 20px;
+}
+
+.primary-btn,
+.secondary-btn {
+    width: max-content;
+    padding: 10px 40px;
+    cursor: pointer;
+    background-color: var(--main-color);
+    color: #fdfdfd;
+    transition: all 0.3s;
+    border: transparent;
+    border-radius: 5px;
+    font-family: inherit;
+}
+
+.primary-btn {
+    width: 100%;
+}
+
+.primary-btn:hover,
+.secondary-btn:hover {
+    background-color: #4737a3;
+}
+
+.secondary-btn {
+    border: 1px solid #fdfdfd;
+}
+
+.edit-dialog {
+    border-radius: 10px;
+    border: 2px solid var(--main-color);
+    padding: 30px;
+    background-color: #f8f6ff;
+    width: 550px;
 }
 </style>
